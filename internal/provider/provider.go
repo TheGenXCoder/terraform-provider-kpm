f30fe9de0c7e348f60d49fb8cee0b20bab693a13
TheGenXCoder <devopsbert@gmail.com>
2026-05-12 10:54:35 -0600

    fix(provider): return non-nil KPMProvider stub with interface stubs

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,12 +1,33 @@
 package provider
 
 import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
 )
 
-// New returns a new provider factory function.
+var _ provider.Provider = &KPMProvider{}
+
+// KPMProvider is the Terraform provider implementation. Fully implemented in Task 4.
+type KPMProvider struct{ version string }
+
+// New returns a provider constructor.
 func New(version string) func() provider.Provider {
-	return func() provider.Provider {
-		return nil
-	}
+	return func() provider.Provider { return &KPMProvider{version: version} }
 }
+
+func (p *KPMProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "kpm"
+	resp.Version = p.version
+}
+
+func (p *KPMProvider) Schema(_ context.Context, _ provider.SchemaRequest, _ *provider.SchemaResponse) {}
+
+func (p *KPMProvider) Configure(_ context.Context, _ provider.ConfigureRequest, _ *provider.ConfigureResponse) {
+}
+
+func (p *KPMProvider) Resources(_ context.Context) []func() resource.Resource { return nil }
+
+func (p *KPMProvider) DataSources(_ context.Context) []func() datasource.DataSource { return nil }
